271b7a43dc4f52c281400c6ee3b2b86df0faba5e
osawa-koki <github@osawa.dev>
2023-01-01 23:03:08 +0900

    共通css

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     'bootstrap/dist/css/bootstrap.min.css',
-    '@/assets/css/style.css',
+    '@/assets/css/style.scss',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
