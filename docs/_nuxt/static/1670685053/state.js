1a76d1198b427b5646f786ba56a718a68ac9db19
osawa-koki <github@osawa.dev>
2023-01-01 22:36:36 +0900

    クラシックデプロイ用のファイル消した。

--- docs/_nuxt/static/1670685053/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsimple-nuxt-study.ssg.js\u002F_nuxt\u002Fstatic\u002F1670685053",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fsimple-nuxt-study.ssg.js\u002F",assetsPath:"\u002Fsimple-nuxt-study.ssg.js\u002F_nuxt\u002F",cdnURL:a}}}}(null));
