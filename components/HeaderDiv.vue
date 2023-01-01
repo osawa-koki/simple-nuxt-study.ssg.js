f74df19750c40672ad1b48276075f030b15f7bc4
osawa-koki <github@osawa.dev>
2023-01-01 22:42:04 +0900

    ヘッダ。

--- components/HeaderDiv.vue
+++ components/HeaderDiv.vue
@@ -1,5 +1,27 @@
 <template>
   <header>
-    
+    <nav>
+      <ul>
+        <li v-for="page in pages" :key="page.name">
+          <nuxt-link :to="page.path">
+            {{ page.name }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
+
+<script>
+import pages from '~/pages';
+
+export default {
+  name: 'HeaderDiv',
+  data() {
+    return {
+      pages,
+    }
+  },
+}
+</script>
+
