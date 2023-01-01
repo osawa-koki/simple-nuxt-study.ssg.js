d85190e5e5d96735f7be945b7fae9c4ffa43a7cc
osawa-koki <github@osawa.dev>
2023-01-01 23:03:44 +0900

    インデックスページ。

--- components/HeaderDiv.vue
+++ components/HeaderDiv.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <nav>
-      <ul>
+      <ul id="Links">
         <li v-for="page in pages" :key="page.name">
           <nuxt-link :to="page.path">
             {{ page.name }}
@@ -24,4 +24,27 @@ export default {
   },
 }
 </script>
+
+<style lang="scss">
+#Links {
+  margin-top: 3rem;
+  display: flex;
+  a {
+    margin-right: 0.5rem;
+    margin-bottom: 1rem;
+    padding-right: 0.5rem;
+    border-right: 1px lightgray solid;
+    color: #0E6DFE;
+    text-decoration: none;
+    &:last-child {
+      margin-right: 0;
+      padding-right: 0;
+      border-right: none;
+    }
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div>
+    <HeaderDiv />
+  </div>
 </template>
 
 <script>
